07a821dda48f1cad590d211fb321370dc666855c
Kotha Divya Meghana <163972501+Dorami611@users.noreply.github.com>
2025-03-22 07:14:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+// Song list
+const songs = [
+    {
+        title: "BELIEVER",
+        artist: "Imagine Dragons",
+        src: "https://appsonwindows.com/apk/8383446/",
+        cover: "https://i.ytimg.com/vi/F8OrAL-PtlE/maxresdefault.jpg"
+    },
+    {
+        title: "BLANK SPACE",
+        artist: "Taylor Swift",
+        src: "song2.mp3",
+        cover: "https://i.ytimg.com/vi/z0PlHSOdp44/maxresdefault.jpg"
+    },
+    {
+        title: "FRI(END)S",
+        artist: "V(BTS)",
+        src: "song3.mp3",
+        cover: "https://i.ytimg.com/vi/dyZkMmTJ8Lw/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGBMgNCh_MA8=&rs=AOn4CLCe6BqxlI4-VEcAjmw5Mmt_VvQY4w"
+    }
+];
+let songIndex = 0;
+
+const audio = document.getElementById('audio');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const cover = document.getElementById('cover');
+
+function loadSong(song) {
+    title.innerText = song.title;
+    artist.innerText = song.artist;
+    audio.src = song.src;
+    cover.src = song.cover;
+}
+
+// Load initial song
+loadSong(songs[songIndex]);
+
+// Play/Pause functionality
+function playSong() {
+    audio.play();
+    playBtn.innerText = "Pause";
+}
+
+function pauseSong() {
+    audio.pause();
+    playBtn.innerText = "Play";
+}
+
+playBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        playSong();
+    } else {
+        pauseSong();
+    }
+});
+
+// Next Song
+function nextSong() {
+    songIndex++;
+    if (songIndex >= songs.length) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Previous Song
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Button event listeners
+nextBtn.addEventListener('click', nextSong);
+prevBtn.addEventListener('click', prevSong);
